becec384cc21bef4119020d4c7668776b6308ec7
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-12 12:17:30 -0500

    No path move

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ time=$(date)
 echo "::set-output name=time::$time"
 echo "My Test $INPUT_REALM"
 echo "Docker Build Complete"
-
-cd ..
 ls
 
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
