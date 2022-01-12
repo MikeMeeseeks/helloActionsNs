0dcbc9ef7242f3df93f71e164bd69845024c79af
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-12 12:03:15 -0500

    Need to find dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ echo "::set-output name=time::$time"
 echo "My Test $INPUT_REALM"
 echo "Docker Build Complete"
 
+cd ..
+cd ..
+ls
 
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
