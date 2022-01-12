8c5006dc6c88eb5ee64bbebcc2921787d93c1a17
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-12 13:06:05 -0500

    Node 14 is correct now

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ node -v
 echo "Hello World $1"
 time=$(date)
 echo "::set-output name=time::$time"
+echo "My Test $INPUT_REALM"
+pwd
+cd /usr/lib/node_modules/@oracle/suitecloud-cli 
+npm install
 
+cd "/github/workspace"
+pwd
+ls
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
 
