bf0d7438637384dadaeb4284972cbfb39e56be7b
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-12 12:41:41 -0500

    WHY

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ echo "Hello World $1"
 time=$(date)
 echo "::set-output name=time::$time"
 echo "My Test $INPUT_REALM"
-dir=$(PWD)
 pwd
 cd /usr/lib/node_modules/@oracle/suitecloud-cli 
 npm install
 
-cd $dir
+cd "/github/workspace"
 pwd
 ls
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
