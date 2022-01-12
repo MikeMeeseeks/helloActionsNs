d6e03dbf0cc2a728b4817da235becec6a010fa9a
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-12 12:28:21 -0500

    Finding directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ echo "Hello World $1"
 time=$(date)
 echo "::set-output name=time::$time"
 echo "My Test $INPUT_REALM"
-echo "Docker Build Complete"
 dir=$(PWD)
+pwd
 cd /usr/lib/node_modules/@oracle/suitecloud-cli 
 npm install
 
 cd $dir
 
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
+
+echo "Docker Build Complete"
