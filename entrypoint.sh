74062887b8953406c4d32673e132d2d03dbdb06b
Mike Rodriguez <michael.rodriguez@shawinc.com>
2022-01-18 11:04:33 -0500

    Revert changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 node -v
-
 time=$(date)
 echo "::set-output name=time::$time"
-echo $gitub.ref
 
 cd /usr/local/lib/node_modules/@oracle/suitecloud-cli
 npm install
 
 cd "/github/workspace"
-pwd
-ls
+
 suitecloud account:savetoken --account $INPUT_REALM --authid helloWorld --tokenid $INPUT_TOKEN_ID --tokensecret $INPUT_TOKEN_SECRET
 
 suitecloud project:deploy
